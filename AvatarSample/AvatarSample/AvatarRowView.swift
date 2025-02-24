9abfb70c858e421e45b093a3901f40d3dff009dd
wonkwh <wonkwh@nc-and.com>
2025-02-24 19:53:57 +0900

    refactor view

---
+++ AvatarSample/AvatarSample/AvatarRowView.swift
@@ -0,0 +1,19 @@
+      HStack(alignment: .top) {
+        Image(person.profileImageName)
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+          .frame(width: 75, height: 75, alignment: .center)
+          .clipShape(.circle)
+          .accessibilityLabel(person.fullName)
+
+        VStack(alignment: .leading) {
+          Text(person.fullName)
+            .font(.headline)
+          Text(person.jobtitle)
+            .font(.subheadline)
+          Text(person.affiliation)
+            .font(.subheadline)
+        }
+      }
+      .accessibilityElement(children: .contain)
+      .accessibilityLabel(person.fullName)
