d3fc4771f7ccfc8b90bc3ecb491ac5234d14b01c
wonkwh <wonkwh@nc-and.com>
2025-03-03 11:59:41 +0900

    ✨ add action handler

--- AvatarSample/AvatarSample/AvatarRowView.swift
+++ AvatarSample/AvatarSample/AvatarRowView.swift
@@ -7,6 +7,10 @@
 //
 import SwiftUI
 
+struct AvatarEditProfileHandler: EnvironmentKey {
+  static var defaultValue: (() -> Void)?
+}
+
 enum AvatarImageShape {
   case round
   case square
@@ -17,6 +21,11 @@ struct AvatarImageShapeKey: EnvironmentKey {
 }
 
 extension EnvironmentValues {
+  var editProfileHandler: (() -> Void)? {
+    get { self[AvatarEditProfileHandler.self] }
+    set { self[AvatarEditProfileHandler.self] = newValue }
+  }
+  
   var avatarImageShape: AvatarImageShape {
     get { self[AvatarImageShapeKey.self] }
     set { self[AvatarImageShapeKey.self] = newValue }
@@ -27,10 +36,16 @@ extension View {
   func avatarImageShape(_ shape: AvatarImageShape) -> some View {
     environment(\.avatarImageShape, shape)
   }
+  
+  public func onEditProfile(handler: @escaping () -> Void) -> some View {
+    environment(\.editProfileHandler, handler)
+  }
 }
 
 struct AvatarRowView: View {
   @Environment(\.avatarImageShape) var imageShape
+  @Environment(\.editProfileHandler) var editProfileHandler
+  
   var person: Person
   
   @ViewBuilder
@@ -64,6 +79,9 @@ struct AvatarRowView: View {
   var body: some View {
     HStack(alignment: .top) {
       profileImage
+        .onTapGesture {
+          editProfileHandler?()
+        }
       VStack(alignment: .leading) {
         titleLabel
         detailsLabel(person.jobtitle)
@@ -80,6 +98,9 @@ struct AvatarRowView: View {
   AvatarRowView(person: Person.sample)
   AvatarRowView(person: Person.sample)
     .avatarImageShape(.round)
+    .onEditProfile {
+      print("Edit Profile")
+    }
     .padding()
   AvatarRowView(person: Person.sample)
     .avatarImageShape(.square)
